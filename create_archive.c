427df715ed3c6e26cdf0285737342f316eded657
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2023-05-18 23:28:56 -0700

    continue writing header

--- create_archive.c
+++ create_archive.c
@@ -10,6 +10,8 @@
 
 Header* create_header(struct dirent* file_entry) {
     int i;
+    int linkname_len;
+    char *uname, *gname
     struct stat* file_stat;
     char* file_name = file_entry -> d_name;
 
@@ -38,7 +40,80 @@ Header* create_header(struct dirent* file_entry) {
     }
 
     /*--------MODE----------*/
-    
+    sprintf(header -> mode, "%07o", file_stat -> st_mode); /* print mode into header */
+    (header -> mode)[7] = "\0";
+
+    /*-------UID AND GID ------*/
+    sprintf(header -> uid, "%07o", file_stat -> st_uid);
+    sprintf(header -> gid, "%07o", file_stat -> st_gid);
+    (header -> uid)[7] = "\0";
+    (header -> gid)[7] = "\0";
+
+    /*-------SIZE------*/
+    sprintf(header -> size, "%011o", file_stat -> st_size);
+
+    /*-------MTIME------*/
+    sprintf(header -> mtime, "%011o", file_stat -> st_mtimespec);
+    (header -> mtime)[11] = "\0";
+
+    /*------CHKSUM ------*/
+
+    /*------FILETYPE, size for symlinks and dirs, linkname ------*/
+    if(S_ISREG(file_stat -> st_mode)) {
+        (header -> typeflag)[0] = REG_FILE_TYPE; /* typeflag = 0 */
+    }
+    else if(S_ISLNK(file_stat -> st_mode)) {
+        (header -> typeflag)[0] = SYM_LINK_TYPE; /* typeflag = 2 */
+        strcpy(header -> size, "000000000000"); /* size of symlinks is 0 */
+        /* if symlink, set linkname to value of it*/
+        linkname_len = readlink(file_name, header -> linkname, file_name, LINKNAME_SIZE);
+        if(linkname_len < LINKNAME_SIZE) {
+            (header -> linkname)[linkname_len - 1] = '\0'; /* add null terminator if there's space*/
+        }
+    }
+    else if(S_ISDIR(file_stat -> st_mode)) {
+        (header -> typeflag)[0] = DIRECTORY_TYPE; /* typeflag = 5 */
+        strcpy(header -> size, "000000000000"); /* size of directory is 0 */
+    }
+
+    /*------MAGIC ------*/
+    strcpy(header -> magic, "ustar");
+
+    /*------VERSION ------*/
+    strcpy(header -> version, "00");
+
+    /*----------UNAME------------*/
+    /* translate uid into name and gid into name*/
+    uname = getpwuid(file_stat -> st_uid);
+    strcpy(header -> uname, uname);
+
+    /*----------GNAME------------*/
+    gname = getgrgid(file_stat -> st_gid);
+    strcpy(header -> gname, gname);
+
+    /*----------DEVMAJOR------------*/
+
+
+    /*----------DEVMINOR------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- create_archive.h
+++ create_archive.h
@@ -1,11 +1,26 @@
 #ifndef __CREATE_ARCHIVE_H__
 #define __CREATE_ARCHIVE_H__
 
-#define NAME_SIZE 100;
-#define MODE_SIZE 8;
-#define UID_SIZE 8;
-#define GID_SIZE 8;
-#define SIZE_SIZE 12;
+#define NAME_SIZE 100
+#define MODE_SIZE 8
+#define UID_SIZE 8
+#define GID_SIZE 8
+#define SIZE_SIZE 12
+#define MTIME_SIZE 12
+#define CHKSUM_SIZE 8
+#define TYPEFLAG_SIZE 1
+#define LINKNAME_SIZE 100
+#define MAGIC_SIZE 6
+#define VERSION_SIZE 2
+#define UNAME_SIZE 32
+#define GNAME_SIZE 32
+#define DEVMAJOR_SIZE 8
+#define DEVMINOR_SIZE 8
+#define PREFIX_SIZE 155
+
+#define REG_FILE_TYPE '0'
+#define SYM_LINK_TYPE '2'
+#define DIRECTORY_TYPE '5'
 
 
 typedef struct header {
